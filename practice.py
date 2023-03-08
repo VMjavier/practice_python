8891f133c97c69cdc156d3b6d565606fd236ac75
Blackfox <110652153+VMjavier@users.noreply.github.com>
2023-03-07 19:29:25 -0500

    preview 1 of Python practices

--- practice.py
+++ practice.py
@@ -1,10 +1,22 @@
 """
 print("hola mundo")
 print(type(5))
-"""
+
 
 # Variables en una sola linea
-
 name, surname, age = "Javier", "Velasco", 35
 print(name, surname, age)
+
+#inputs
+age = input("Cuál es tu edad? \n")
+print(age)
+"""
+
+#operators
+x1 = float(input("Enter the first number: "))
+x2 = float(input("Enter the second number: "))
+
+print("the sum of the two numbers is: ", x1+x2)
+print("the rest of the two number is: ", x1-x2)
+
 
