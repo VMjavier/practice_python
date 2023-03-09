0564e0a328faaf22b92a59248f69367250bcf5bf
Blackfox <110652153+VMjavier@users.noreply.github.com>
2023-03-08 21:48:18 -0500

    Advance 3 of Python practice.

--- practice.py
+++ practice.py
@@ -36,6 +36,7 @@ print(x1<=x2)
 print(x1!=x2)
 """
 
+"""
 #formateo
 name, surname, age = "Javier", "Velasco", 35
 print("My name is {} {}. I'm {} year old".format(name,surname, age))
@@ -43,3 +44,22 @@ print("My name is %s %s. I'm %d year old" %(name, surname, age))
 print("My name is "+name+" "+surname+". I'm "+ str(age)+" year old")
 print(f"My name is {name} {surname}. I'm {age} year old")
 
+
+
+#string, optional "
+multiline_string = '''I am a teacher math.
+I'm from Pamplona - Colombia.'''
+print(multiline_string)
+
+#Concatenated
+animal = ["Cat", "Dog", "Cow", "Donkey", "Alpaca"]
+result = ' '.join(animal)
+print(result)
+result = ' - '.join(animal)
+print(result)
+"""
+
+#strip(): Removes all given characters starting from the beginning and end of the string
+animal = "loscamareroslocos"
+result = animal.strip("sol")
+print(result)
